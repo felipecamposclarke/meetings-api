06a4ba82c0891ec4b4edf6d8baee159c3c5dd5b5
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2014-12-07 17:13:40 -0300

    chore(): create seed from the meeting model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+def create_meeting(title)
+  Meeting.create(
+    title: title,
+    description: Faker::Lorem.words(12).join(" "),
+    author: Faker::Name.name,
+    date: Faker::Date.forward(10),
+  )
+end
+
+
+[*10..15].sample.times do |n|
+  create_meeting "Meeting #{n}"
+end
